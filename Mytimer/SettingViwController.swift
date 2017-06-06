b036a118f2d686aab3616bfd13a0f0dea4adf3d8
浅野未央 <mio@asanomionoMacBook-Pro.local>
2017-06-06 19:59:09 +0900

    Initial Commit

---
+++ Mytimer/SettingViwController.swift
@@ -0,0 +1,84 @@
+//
+//  SettingViwController.swift
+//  Mytimer
+//
+//  Created by 浅野未央 on 2017/06/03.
+//  Copyright © 2017年 mio. All rights reserved.
+//
+
+import UIKit
+
+class SettingViwController: UIViewController ,UIPickerViewDataSource ,UIPickerViewDelegate {
+
+  let settingArrey : [Int] = [10,20,30,40,50,60]
+  let settingKey = "time_value"
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      timerSettingPicker.delegate = self
+      timerSettingPicker.dataSource = self
+      
+      let settings = UserDefaults.standard
+      let timerValue = settings.integer(forKey: settingKey)
+      
+      //40だったら４０の戻る
+      for row in 0..<settingArrey.count{
+        if settingArrey[row] == timerValue {
+          timerSettingPicker.selectRow(row, inComponent:0 ,animated:true)
+        }
+        
+      }
+
+      
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+  @IBOutlet weak var timerSettingPicker: UIPickerView!
+
+  @IBAction func decButtonAction(_ sender: Any) {
+   
+    _ = navigationController?.popViewController(animated: true)
+    
+  }
+  func numberOfComponents(in pickerView: UIPickerView) -> Int{
+    return 1
+    
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return settingArrey.count
+  }
+//見えてるとこしか呼ばれない
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return String(settingArrey[row])
+  }
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    let settings = UserDefaults.standard
+    settings.setValue(settingArrey[row], forKey: settingKey)
+    settings.synchronize()
+  }
+  }
+  
+  //synchonize
+  
+
+
+
+
---
+++ Mytimer/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  Mytimer
+//
+//  Created by 浅野未央 on 2017/06/03.
+//  Copyright © 2017年 mio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  var timer : Timer?
+  
+  var count = 0
+  //　int型
+		
+  let settingKey = "timer_value"
+		
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    let settings = UserDefaults.standard
+    //起動時のデフォルト（初期値；設定値がない場合を登録）
+    settings.register(defaults: [settingKey:10])
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  @IBOutlet weak var conuntDuwnLabel: UILabel!
+  
+  @IBAction func settingBottonAction(_ sender: Any) {
+    
+    if let nowTimer = timer {
+      //アンラップ
+      
+      if nowTimer.isValid == true{
+        nowTimer.invalidate()
+      }
+      
+    }
+    //矢印の動作
+   performSegue(withIdentifier: "goSetting", sender: nil)
+  }
+  
+  @IBAction func startButtonAction(_ sender: Any) {
+    
+    //連打した場合のイレギュラー処理
+    if let nowTimer = timer {
+    if nowTimer.isValid == true{
+      
+        return
+      }
+    }
+    
+    timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                 target: self,
+                                 selector: #selector(self.tiameImterrupt(_timer:)),
+                                 userInfo: nil,
+                                 repeats: true)
+                                   //アラートはflse
+
+  }
+  
+  @IBAction func stopButtonAction(_ sender: Any) {
+    if let nowTimer = timer {
+      
+      if nowTimer.isValid == true{
+        nowTimer.invalidate()
+        
+      }
+    }
+    
+  }
+  
+  func displayUpdate() -> Int {
+    
+    let settings = UserDefaults.standard
+    
+    let timerValue = settings.integer(forKey: settingKey)
+    
+    let remainCount = timerValue - count
+    
+    conuntDuwnLabel.text = "残り\(remainCount)秒"
+    
+    return remainCount
+    
+    
+  }
+  func  tiameImterrupt(_timer:Timer) {
+    
+    count += 1
+    if displayUpdate() <= 0 {
+      
+      count = 0
+      
+      timer?.invalidate()
+    }
+    
+  }
+  override func viewDidAppear(_ animated: Bool) {
+   count = 0
+    
+    _ = displayUpdate()
+  }
+  
+}
+
